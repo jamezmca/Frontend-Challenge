608ae39ad9ffa601bd57a37a0267fb8c815769e9
James <jamesmca@live.com>
2021-10-25 20:45:12 -0700

    compile contract time

--- nft-marketplace/src/App.js
+++ nft-marketplace/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { ethers } from 'ethers'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+
+import Home from "./components/Home";
+import MarketPlace from "./components/MarketPlace";
+import './App.css'
+
+const contractAddress = '0xDb172Cf85521EBA2d825C4DD5824ef878696Ee05'
+
+
 
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav >
+          <ul className="navbar">
+            <li>
+              <Link to="/">Create NFT</Link>
+            </li>
+            <li>
+              <Link to="/marketplace">Market Place</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+          <Route path="/marketplace">
+            <MarketPlace />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
